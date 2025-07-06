ad8ce1d79334061981dd73c185b9fe2d012fbcc3
Rebeca Vitória <rebeca.coutinho@serpro.gov.br>
2025-07-05 23:05:15 -0300

    todo: AWS SQS configuration

---
+++ src/main/java/com/becxagy/book/api/infra/aws/sqs/AmazonSQSConfig.java
@@ -0,0 +1,32 @@
+package com.becxagy.book.api.infra.aws.sqs;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonSQSConfig {
+
+    @Value("${aws.access-key}")
+    private String accessKey;
+
+    @Value("${aws.secret-key}")
+    private String secretKey;
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Bean
+    public AmazonSQSAsync amazonSQSAsync() {
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        return AmazonSQSAsyncClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+    }
+}
